325d91829a7a4e38e76800abdb6339d1327fe885
Pulkit Goyal <pulkitcgoyal@gmail.com>
2024-12-31 15:52:14 +0530

    add sign in form

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -7,3 +7,9 @@ export const SignupValidation = z.object({
     email : z.string().email({message:'Invalid email'}),
     password: z.string().min(8,{message:'Password must be at least 8 characters long'}),
 });
+
+
+export const SigninValidation = z.object({
+    email : z.string().email({message:'Invalid email'}),
+    password: z.string().min(8,{message:'Password must be at least 8 characters long'}),
+});
