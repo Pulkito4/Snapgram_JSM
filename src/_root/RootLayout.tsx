0614d96b1d2063f54d3eb686d5fde22147d1f3b0
Pulkit Goyal <pulkitcgoyal@gmail.com>
2025-01-01 21:51:21 +0530

    add top bar to home page

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import BottomBar from "@/components/shared/BottomBar";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import Topbar from "@/components/shared/Topbar";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
-  return (
-    <div>RootLayout</div>
-  )
-}
+	return (
+		<div className="w-full md:flex">
+			<Topbar />
+			<LeftSideBar />
 
-export default RootLayout
+			<section className=" flex flex-1 h-full">
+				<Outlet />
+			</section>
+			<BottomBar />
+		</div>
+	);
+};
+
+export default RootLayout;
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ import {
     useQueryClient,
     useInfiniteQuery,
 } from '@tanstack/react-query';
-import { createUserAccount, signInAccount } from '../appwrite/api';
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
 import { INewUser } from '@/types';
 
 export const useCreateUserAccountMutation = () => {
@@ -18,7 +18,15 @@ export const useCreateUserAccountMutation = () => {
 export const useSignInAccountMutation = () => {
     return useMutation(
         {
-            mutationFn: (user: {email:string, password:string}) => signInAccount(user),
+            mutationFn: (user: { email: string, password: string }) => signInAccount(user),
+        }
+    );
+};
+
+export const useSignOutAccountMutation = () => {
+    return useMutation(
+        {
+            mutationFn: signOutAccount,
         }
     );
 };
