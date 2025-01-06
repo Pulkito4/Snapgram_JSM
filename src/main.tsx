f5d19840ebb8d6bc17853dcc9375f8ff9765ac76
Pulkit Goyal <pulkitcgoyal@gmail.com>
2025-01-07 01:33:50 +0530

    add web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext.tsx";
 import { QueryProvider } from "./lib/react-query/QueryProvider.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
 			<QueryProvider>
 				<AuthProvider>
 					<App />
+					<Analytics />
 				</AuthProvider>
 			</QueryProvider>
 		</BrowserRouter>
